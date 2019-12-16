a4c9452515b2ec54ab038acf4a85a936510b24e1
mushi1002 <mahmud.mushfique@gmail.com>
2019-12-16 16:33:59 +0600

    using Leipzig Corpora Collection to increase the capability of the database

--- chatbot.py
+++ chatbot.py
@@ -7,10 +7,11 @@ import numpy as np
 import random
 import string
 
-
 #corpus code
-
-f=open('chatbot.txt','r',errors='ignore')
+name_of_the_corpus="corpus-large.txt"
+#name_of_the_corpus="corpus-medium.txt"
+#name_of_the_corpus="corpus-small.txt"
+f=open(name_of_the_corpus,'r',errors='ignore')
 raw=f.read()
 raw=raw.lower()
 sent_tokens=nltk.sent_tokenize(raw)
@@ -49,19 +50,19 @@ def response(user_response):
     
     vals = cosine_similarity(tfidf[-1], tfidf)
     idx=vals.argsort()[0][-2] # sorting the values and returning the index serial. it is a [[,,,]] matrix 
-    #and getting [0][-2] means getting the second largest element in it's only row (0 th row). largest element is always 1 as userinput 
+    # and getting [0][-2] means getting the second largest element's index in it's only row (0 th row). largest element is always 1 as userinput 
     # is inside sent_tokens
  
     flat = vals.flatten()
     #this line converts the 2d array into a 1d array. that means [[]] -> []
 
     flat.sort()
-    req_tfidf = flat[-2] #second largest element of flat. 
+    req_tfidf = flat[-2] #second largest element of flat 1d matrix . 
     if(req_tfidf==0):
         robo_response=robo_response+"I am sorry! I don't understand you"
         return robo_response
     else:
-        robo_response = robo_response+sent_tokens[idx]
+        robo_response = robo_response+sent_tokens[idx][6:]
         return robo_response
     
     
@@ -74,14 +75,14 @@ while(flag==True):
     if(user_response!='bye'):
         if(user_response=='thanks' or user_response=='thank you' ):
             flag=False
-            print("ROBO: You are welcome..")
+            print("MARK: You are welcome..")
         else:
             if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
+                print("MARK: "+greeting(user_response))
             else:
-                print("ROBO: ",end="")
+                print("MARK: ",end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else:
         flag=False
-        print("ROBO: Bye! take care..")
+        print("MARK: Bye! take care..")
